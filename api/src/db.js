8661b3b4d1751a5328af7b6ef20c746804bfd9d0
Fatimalezcano <fatimalezcanodev@gmail.com>
2023-02-28 19:00:10 -0300

    cambios deploy

--- api/src/db.js
+++ api/src/db.js
@@ -4,16 +4,21 @@ const fs = require("fs");
 const path = require("path");
 
 const { DATABASE_URL } = process.env;
-const sequelize = new Sequelize(DATABASE_URL, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
+
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
-  },
-});
+  }
+);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
